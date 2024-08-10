c93d27898f4580645ad81c991200217fd2b70057
subhash birajdar <subhashchandrabirajdar98@gmail.com>
2024-08-10 08:46:01 +0530

    Handle LoginException

---
+++ src/main/java/com/foodApp_UserLogin/exception/LoginException.java
@@ -0,0 +1,13 @@
+package com.foodApp_UserLogin.exception;
+
+public class LoginException extends RuntimeException{
+
+    public LoginException() {
+        // TODO Auto-generated constructor stub
+    }
+
+
+    public LoginException(String message) {
+        super(message);
+    }
+}
